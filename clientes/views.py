0ac2b0c7be9c7a2ff8b3e178dcbe58aee3f66f73
Suleiman Moraes <suleimanmoraes@gmail.com>
2018-12-11 10:08:50 -0200

    atualização

--- clientes/views.py
+++ clientes/views.py
@@ -6,7 +6,12 @@ from .forms import PersonForm
 
 @login_required
 def persons_list(request):
-    persons = Person.objects.all()
+    termo_busca = request.GET.get('pesquisa', None)
+    if termo_busca:
+        persons = Person.objects.filter(first_name__contains=termo_busca)
+    else:
+        persons = Person.objects.all()
+
     return render(request, 'person.html', {'persons': persons})
 
 
